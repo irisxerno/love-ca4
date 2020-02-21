797d8184d439b43ee99d6cac463d2e5be97b9e8a
My Name Is (name) <antsuke42@gmail.com>
2020-02-21 09:24:56 +0200

    tee

--- main.lua
+++ main.lua
@@ -1,5 +1,10 @@
 success = love.window.setMode(731, 411)
+
+--
 -- utils
+--
+
+color = require "color"
 
 function RGB(r,g,b)
   if type(r)=="table" then
@@ -7,7 +12,8 @@ function RGB(r,g,b)
   end
   return r/255,g/255,b/255
 end
-color = require "color"
+
+
 function get_color(i,a)
   local r,g,b
   if a then
@@ -17,6 +23,7 @@ function get_color(i,a)
     return RGB(color[i])
   end
 end
+
 function set_color(i,a)
   if type(a)=="boolean" and a then
     love.graphics.setBackgroundColor(get_color(i))
@@ -24,19 +31,36 @@ function set_color(i,a)
     love.graphics.setColor(get_color(i,a))
   end
 end
+
 font = love.graphics.newFont("SourceCodePro-Bold.ttf",20)
 love.graphics.setFont(font)
 
 function clamp(i,min,max)
   return math.max(math.min(i,max),min)
 end
+
+--
 -- "assets" and "design"
+--
 
 a = {}
 a.sym = {"β","δ","λ","φ"}
 a.symc = {9,10,12,13}
 a.num = {"1","2","3","4","5","6","7","8","9","x","J","Q","K"}
 a.stats_ord = { "mxa", "hs", "d", "xp" }
+a.draw_data = {{3534, 2426, 1632, 1082, 658, 339, 187, 102, 35, 5, 0, 0}, {615, 1360, 1728, 1757, 1519, 1165, 864, 543, 310, 105, 32, 2}, {43, 315, 681, 1076, 1374, 1566, 1508, 1288, 1082, 692, 323, 52}, {1, 32, 136, 288, 565, 860, 1225, 1497, 1772, 1668, 1435, 521}, {0, 1, 9, 35, 93, 200, 364, 609, 1045, 1649, 2467, 3528}}
+
+function a.stat_cost(s)
+  if type(s)=="number" then
+    s = a.stats_ord[s]
+  end
+  local c = g.stats[s]
+  if s == "mxa" then
+    c=c*5
+  end
+  return c
+end
+
 d = {}
 d.w = love.graphics.getWidth()
 d.h = love.graphics.getHeight()
@@ -46,16 +70,22 @@ d.touch = {}
 d.touch.selarea = 20
 d.stats = { x = 600, y = 20 }
 d.stat = { w = 60, h = 48 }
-d.map = { x = 60, y = 20 }
+d.map = { x = 100, y = 10 }
 d.tile = { w = 60, h = 52 }
+d.xp_held_s = 15
 
 function triangle(x,y,w,h,ss)
   local p1 = { x,y+h, x+w,y+h, x+w/2,y }
   local p2 = { x+8,y+h-4, x+w-8,y+h-4, x+w/2,y+8 }
-  set_color(5,0.5)
-  love.graphics.polygon("fill",p1)
-  set_color(1)
-  love.graphics.polygon("line",p2)
+    set_color(7,0.5)
+    love.graphics.polygon("fill",p1)
+  if ss == 2 then
+    set_color(1)
+    love.graphics.polygon("line",p2)
+  elseif ss == 1 then
+    set_color(1,0.5)
+    love.graphics.polygon("line",p2)
+  end
 end
 
 function rectangle(x,y,w,h,c,s)
@@ -78,6 +108,19 @@ function new_card()
            sel = false }
 end
 
+function new_tile(r)
+  local dd = a.draw_data[r]
+  local o = 10000
+  local n = love.math.random(1,o)
+  for i=1,12 do
+    if n>dd[i] then
+      n=n-dd[i]
+    else
+      return i+1
+    end
+  end
+end
+
 function reset()
   g = {}
   g.hand = {}
@@ -86,15 +129,18 @@ function reset()
   g.progress = { i = 0, ac = 0 }
   g.map = {}
   g.mapv = {}
-  for i=1,5 do
-    g.map[i] = {}
-    g.mapv[i] = {}
-    for j=1,i do
-      g.map[i][j] = 0
-      g.mapv[i][j] = 0
+  for r=1,5 do
+    g.map[r] = {}
+    g.mapv[r] = {}
+    for c=1,(6-r) do
+      g.map[r][c] = 0
+      g.mapv[r][c] = 0
     end
   end
-  for i=1,10 do
+  for c=1,5 do
+    g.mapv[1][c] = 2
+  end
+  for i=1,5 do
     g.hand[i] = new_card()
   end
 end
@@ -103,6 +149,7 @@ reset()
 held = nil
 held_gi = nil
 xp_gi = false
+
 
 function unfetter_card(c)
   return { num = c.num, sym = c.sym, sel = false }
@@ -129,10 +176,13 @@ function getelem(x,y,next)
   return nil
 end
 
-function getstat(x,y)
+function getstat(x,y,ss)
   lx = (x-d.stats.x)/d.stat.w
   ly = (y-d.stats.y)/d.stat.h+1
   if lx >= 0 and lx <= 1 and ly >= 1 and ly <= 5 then
+    if ss then
+      return math.floor(ly)
+    end
     return a.stats_ord[math.floor(ly)]
   end
   return nil
@@ -147,10 +197,9 @@ function love.update(dt)
     y-d.touch.selarea<touch.y and y+d.touch.selarea>touch.y) and touch.state == "touch" then
       touch.state = "drag"
       ei,source = getelem(touch.x, touch.y)
-      if ei then
+      if ei and source then
         e = g[source][ei]
         if e.sel then
-          -- TODO: select many cards
           held = {}
           new_hand = {}
           for i,cd in ipairs(g.hand) do
@@ -168,6 +217,8 @@ function love.update(dt)
           held = { e }
           table.remove(g[source], ei)
         end
+      elseif getstat(touch.x,touch.y) == "xp" then
+        xp_held = true
       end
     elseif touch.state == "drag" and held then
       gi = getelem(x,y,true)
@@ -178,11 +229,21 @@ function love.update(dt)
         xp_gi = true
       end
     end
+    if xp_held then
+      gs = getstat(x,y,true)
+      if gs and gs < 4 then
+        xp_held = gs
+      else
+        xp_held = true
+      end
+    end
   end
 end
 
 function love.mousepressed(x,y,button)
-  print(getstat(x,y))
+  if x>0 and y>0 and x<50 and y<50 and false then
+    table.insert(g.hand,new_card())
+  end
   if button == 1 then
     touch.x, touch.y = love.mouse.getPosition()
     touch.state = "touch"
@@ -192,115 +253,124 @@ end
 function love.mousereleased(x,y,button)
   if button == 1 then
     if touch.state == "touch" then
-      ei,source = getelem(x,y)
+      local ei,source = getelem(x,y)
       if ei and ei == getelem(touch.x, touch.y) then
         e = g[source][ei]
         e.sel = not e.sel
       end
-    elseif touch.state == "drag" and held then
-      if getstat(x,y)=="xp" then
-        g.stats.xp = g.stats.xp + table.getn(held)
-      elseif held_gi then
-        for k,cd in ipairs(held) do
-          table.insert(g.hand,held_gi,unfetter_card(cd))
-          held_gi=held_gi+1
-        end
-      else
-        if table.getn(held) == 1 and held[1].index then
-          table.insert(g[held[1].source],held[1].index,unfetter_card(held[1]))
-        else
+    elseif touch.state == "drag" then
+      if held then
+        if getstat(x,y)=="xp" then
+          g.stats.xp = g.stats.xp + table.getn(held)
+        elseif held_gi then
           for k,cd in ipairs(held) do
-            table.insert(g[cd.source],unfetter_card(cd))
+            table.insert(g.hand,held_gi,unfetter_card(cd))
+            held_gi=held_gi+1
+          end
+        else
+          if table.getn(held) == 1 and held[1].index then
+            table.insert(g[held[1].source],held[1].index,unfetter_card(held[1]))
+          else
+            for k,cd in ipairs(held) do
+              table.insert(g[cd.source],unfetter_card(cd))
+            end
           end
         end
+        held = nil
+      elseif xp_held then
+        local gs = getstat(x,y,true)
+        if gs and gs ~= 4 then
+          if g.stats.xp - a.stat_cost(gs) >= 0 then
+            g.stats.xp = g.stats.xp - a.stat_cost(gs)
+            g.stats[a.stats_ord[gs]] = g.stats[a.stats_ord[gs]] + 1
+          end
+        end
+        xp_held = nil
       end
-      held = nil
     end
   end
 end
 
 -- draw
 function draw_card(cd,x,y)
-  --[[
-  if cd.sel then
-    set_color(1,0.9)
-    love.graphics.rectangle("line", x+2,y+2,d.card.w-4,d.card.h-4)
-    set_color(6,0.5)
-    love.graphics.rectangle("fill", x+4,y+4,d.card.w-8,d.card.h-8)
-  else
-    set_color(7,0.5)
-    love.graphics.rectangle("fill", x,y,d.card.w,d.card.h)
-  end
-  set_color(a.symc[cd.sym])
-  love.graphics.rectangle("line", x+4,y+4,d.card.w-8,d.card.h-8)
-  --]]
   rectangle(x,y,d.card.w,d.card.h,a.symc[cd.sym],cd.sel)
   love.graphics.print(a.num[cd.num],x+(d.card.w/3),y+(d.card.h/5-4))
   love.graphics.print(a.sym[cd.sym],x+(d.card.w/3),y+(d.card.h/2-4))
 end
-function draw_stat(i,c,s,x,y)
-  --[[
-  if s then
-    set_color(1,0.9)
-    love.graphics.rectangle("line", x+2,y+2,d.stat.w-4,d.stat.h-4)
-    set_color(6,0.5)
-    love.graphics.rectangle("fill", x+4,y+4,d.stat.w-8,d.stat.h-8)
-  else
-    set_color(7,0.5)
-    love.graphics.rectangle("fill", x,y,d.stat.w,d.stat.h)
-  end
-  set_color(c)
-  love.graphics.rectangle("line", x+4,y+4,d.stat.w-8,d.stat.h-8)
- --]]
+function draw_stat(i,c,s,x,y,c2)
   rectangle(x,y,d.stat.w,d.stat.h,c,s)
+  if c2 then
+    set_color(c2)
+  end
   love.graphics.print(i,x+(d.stat.w/3),y+(d.stat.h/5))
 end
 
-
 function love.draw()
+  x, y = love.mouse.getPosition()
+  devug = getstat(x,y)
+  if not devug then
+    devug = "nil"
+  end
   set_color(8,true)
   love.graphics.setLineWidth(2)
-  if true then
-    set_color(1)
-    love.graphics.print("tröäänkle\n"..d.h.."+"..d.w.."\n= {β,δ,λ,φ}",0,0)
-  end
-  gc = 0
+  local gc = 0
   for i,cd in ipairs(g.hand) do
     if held_gi and i == held_gi then
       gc = gc + 1
     end
     draw_card(cd,d.hand.x+d.card.w*(i-1+gc),d.hand.y)
   end
+  local sele = nil
   for i,st in ipairs(a.stats_ord) do
-    c = 15
-    t = false
+    local ie = g.stats[st]
+    local c = 15
+    local t = false
     if i == 4 then
       c = 16
       if xp_gi then
         t = true
       end
     end
-    draw_stat(g.stats[st],c,t,d.stats.x,d.stats.y+d.stat.h*(i-1))
+    if i==xp_held then
+      t = true
+      sele = g.stats.xp - a.stat_cost(i)
+    end
+    if sele and st=="xp" then
+      local ce = 9
+      if sele >= 0 then
+        ce = 12
+      end
+      draw_stat(sele,c,t,d.stats.x,d.stats.y+d.stat.h*(i-1),ce)
+    else
+      draw_stat(ie,c,t,d.stats.x,d.stats.y+d.stat.h*(i-1))
+    end
+    if i ~= 4 and xp_held then
+      love.graphics.print(a.stat_cost(i),d.stats.x-d.stat.w*.8,d.stats.y+d.stat.h*(i-1+.1))
+    end
   end
   draw_stat(table.getn(g.drop),6,false,d.stats.x-d.stat.w,d.stats.y+d.stat.h*(3))
-  for i=1,5 do
-    for j=1,i do
-      if g.mapv[i][j] ~= -1 then
-        triangle(d.map.x+d.tile.w*i-(d.tile.w*(j+1)/2), d.map.y+d.tile.h*(5-j), d.tile.w, d.tile.h, g.mapv[i][j])
-        if g.mapv == 2 then
-          love.graphics.print(g.map[i][v])
+  for r=1,5 do
+    for c=1,(6-r) do
+      if g.mapv[r][c] ~= -1 then
+        triangle(d.map.x+d.tile.w*(c-1)+(d.tile.w*(r-1)/2), d.map.y+d.tile.h*(6-r-1), d.tile.w, d.tile.h, g.mapv[r][c])
+        if g.mapv[r][c] == 2 then
+          if g.map[r][c] == 0 then
+            g.map[r][c] = new_tile(r)
+          end
+          love.graphics.print(g.map[r][c],d.map.x+d.tile.w*(c-1)+(d.tile.w*(r-1)/2)+d.tile.w*.3, d.map.y+d.tile.h*(6-r-1)+d.tile.h*.43)
         end
       end
     end
   end
+  x, y = love.mouse.getPosition()
   if held then
     for i,cd in ipairs(held) do
-      x, y = love.mouse.getPosition()
       draw_card(cd,x-d.card.w/2+(i*4), y-d.card.h/2+(i*4))
     end
-  end
-  for i=1,548 do
-    set_color(love.math.random(1,16))
-    love.graphics.points(love.math.random(0,d.w),love.math.random(0,d.h))
+  elseif xp_held then
+    set_color(7,0.5)
+    love.graphics.circle("fill",x,y,d.xp_held_s)
+    set_color(16)
+    love.graphics.circle("line",x,y,d.xp_held_s-4)
   end
 end
